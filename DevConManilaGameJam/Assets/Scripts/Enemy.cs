da9e84c2261b210736963855c072af039def9c54
AldwynnUrbistondo <aldwynn020505@gmail.com>
2025-08-08 20:55:16 +0800

    .

--- DevConManilaGameJam/Assets/Scripts/Enemy.cs
+++ DevConManilaGameJam/Assets/Scripts/Enemy.cs
@@ -3,11 +3,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour, IDamageable
 {
+    [HideInInspector] public Transform playerPos;
+    [HideInInspector] public Rigidbody2D rb;
+
     public float currentHealth;
     public float maxHealth;
-
     public bool isDying;
 
+    public float moveSpeed = 2;
+    [HideInInspector] public bool isFacingRight = true;
+
+    public Transform healthBarCanvas;
+
+    public void Start()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public virtual void Update()
+    {
+        Vector2 direction = (playerPos.position - transform.position).normalized;
+
+        rb.linearVelocity = new Vector2(direction.x * moveSpeed, 0);
+
+        Flip(direction);
+    }
+
+    #region TakeDamage and Die
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
@@ -34,4 +57,25 @@ public class Enemy : MonoBehaviour, IDamageable
         yield return null;
         Destroy(this.gameObject);
     }
+    #endregion
+
+    public void Flip(Vector2 direction)
+    {
+        if ((isFacingRight && direction.x < 0f || !isFacingRight && direction.x > 0f))
+        {
+            isFacingRight = !isFacingRight;
+            Vector2 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+
+
+            if (healthBarCanvas != null)
+            {
+                Vector2 childScale = healthBarCanvas.localScale;
+                childScale.x *= -1f;
+                healthBarCanvas.localScale = childScale;
+            }
+        }
+    }
+
 }
--- DevConManilaGameJam/Assets/Scripts/EnemyMelee.cs
+++ DevConManilaGameJam/Assets/Scripts/EnemyMelee.cs
@@ -2,5 +2,51 @@ using UnityEngine;
 
 public class EnemyMelee : Enemy
 {
-    
+    RaycastHit2D targetRay;
+    public float attackDistance;
+
+    public override void Update()
+    {
+        if (!TargetInRange())
+        {
+            Vector2 direction = (playerPos.position - transform.position).normalized;
+
+            rb.linearVelocity = new Vector2(direction.x * moveSpeed, 0);
+
+            Flip(direction);
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+
+        if (isFacingRight)
+        {
+            Debug.DrawRay(transform.position, transform.right * attackDistance, Color.red);
+
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, -transform.right * attackDistance, Color.red);
+        }
+    }
+
+    public bool TargetInRange()
+    {
+        if (isFacingRight)
+        {
+            targetRay = Physics2D.Raycast(transform.position, transform.right, attackDistance, LayerMask.GetMask("Player"));
+        }
+        else
+        {
+            targetRay = Physics2D.Raycast(transform.position, -transform.right, attackDistance, LayerMask.GetMask("Player"));
+        }
+
+        if (targetRay.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
